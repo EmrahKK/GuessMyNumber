f35d5d603855448c69ea94ced3e732141fa39cfe
emrahkk <emrahkk@gmail.com>
2014-06-25 14:28:10 +0300

    Initial Commit

---
+++ GuessMyNumber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GuessMyNumber
+//
+//  Created by Emrah on 25.06.2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GuessMyNumberTests/GuessMyNumberTests.m
@@ -0,0 +1,34 @@
+//
+//  GuessMyNumberTests.m
+//  GuessMyNumberTests
+//
+//  Created by Emrah on 25.06.2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GuessMyNumberTests : XCTestCase
+
+@end
+
+@implementation GuessMyNumberTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
